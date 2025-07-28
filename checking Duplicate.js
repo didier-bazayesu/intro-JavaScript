cda33f43260d296bf5c6b0e47b7cbcc241a5c623
didier-bazayesu <didierbazayesu@gmail.com>
2025-07-28 10:47:51 +0200

    checking duplicate

---
+++ checking Duplicate.js
@@ -0,0 +1,22 @@
+
+function isIsogram(str){
+         let lower= str.toLowerCase()  // all lower letter
+  
+  let   st = lower.split('').filter((elem,index)=>{
+        if(lower.indexOf(elem) ===  index){
+            return true;  //removing duplicate
+        }
+     })
+        
+        return lower.length === st.length
+
+}
+
+//another way
+function isIsogram(str){
+    let lower = str.split('')
+    let final = new Set(lower)
+    return final.size === lower.length
+}
+console.log(isIsogram('hellp'))
+
