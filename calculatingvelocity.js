4f9ab86757fe5bba67c43500705b542762b79ef9
didier-bazayesu <didierbazayesu@gmail.com>
2025-08-06 10:44:21 +0200

    feat: learning prototype

---
+++ calculatingvelocity.js
@@ -0,0 +1,11 @@
+function sakuraFall(v) {
+    // your code here
+
+    if (v > 0) {
+
+        let distance = 400;  // distance = velocity * time
+        return distance / v
+    } else {
+        return 0.0
+    }
+}
