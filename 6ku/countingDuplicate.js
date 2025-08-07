59e09ea722a4a15886b0e3dcd13774e7a1dea281
didier-bazayesu <didierbazayesu@gmail.com>
2025-08-07 11:14:41 +0200

    feat: adding another way

--- 6ku/countingDuplicate.js
+++ 6ku/countingDuplicate.js
@@ -27,9 +27,49 @@ function duplicateCount(text) {
         }
     }
     return count
+
+    
 }
+
+
 console.log(duplicateCount("aabBcde"))
 
+//another way of using filter
+
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+function removeDuplicate(text) {
+    let f = duplicate = text.toLowerCase().split('')
+    let final = duplicate.filter((elem, index, arr) => {
+        return arr.indexOf(elem) !== index
+
+
+    })
+    return final.length
+
+}
+console.log(removeDuplicate("aaghebhBcde"))
+
+
+
+
+
+
+
+
+
+
+
+////
 
 
 
