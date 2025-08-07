3ac69d9317779f03887b13b50db9bb7ec4a103e5
didier-bazayesu <didierbazayesu@gmail.com>
2025-08-07 10:58:44 +0200

    feat : adding new katas

---
+++ 6ku/compress.js
@@ -0,0 +1,18 @@
+function digitalRoot(n) {
+
+    let f = n.toString().split('')
+        .map(Number).reduce((x, y) => {
+            return x + y
+        })
+
+    while (f > 9) {
+        Number(
+            f = f.toString().split('').map(Number)
+                .reduce((x, y) => x + y))
+    }
+
+    return f;
+
+
+
+}
