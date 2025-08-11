259fc6f3f9da299d36a1654198b13c688187011b
didier-bazayesu <didierbazayesu@gmail.com>
2025-08-11 11:05:46 +0200

    feat: adding code wars for compress nick name

--- 6ku/name Of people.js
+++ 6ku/name Of people.js
@@ -1,1 +1,24 @@
 'https://www.codewars.com/kata/5768a693a3205e1cc100071f'
+
+
+//this katas is for naming style for more than 3 names 
+
+function initializeNames(name) {
+    // Insert your brilliant code her
+
+    let array = name.split(' ')
+    if (array.length > 2) {
+
+        let firstName = array.shift()
+        let lastName = array.pop()
+
+        let final = array.map(el => el.charAt(0) + '.')
+        final.unshift(firstName)
+        final.push(lastName)
+        return final.join(' ')
+    } else {
+        return name;
+    }
+    //  
+
+}
