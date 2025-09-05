8d9d2c71ecb04aa1ced31ae1c2bb803f0d01e7ce
didier-bazayesu <didierbazayesu@gmail.com>
2025-09-06 01:17:37 +0200

    Update PlayingWithSet.js

--- 7ku/PlayingWithSet.js
+++ 7ku/PlayingWithSet.js
@@ -1,10 +1,10 @@
-function areEqual(s1, s2) {
+function areEqual(s1, s3) {
     // ...
-    return [...s1, ...s2].every(ele => s1.has(ele) && s2.has(ele))
+    return [...s1, ...s3].every(ele => s1.has(ele) && s3.has(ele))
 
 }
 
-function notEqual(s1, s2) {
-    return !areEqual(s1, s2)
+function notEqual(s1, s)3 {
+    return !areEqual(s1, s3)
 }
 'https://www.codewars.com/kata/5884e9ccf72916207900010f'
