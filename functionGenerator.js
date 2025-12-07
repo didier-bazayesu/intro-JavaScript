c1de8d60c596cd75624ccb48aadb8efa776a30d2
didier-bazayesu <didierbazayesu@gmail.com>
2025-12-07 20:23:43 +0200

    feat: lerning function generator

---
+++ functionGenerator.js
@@ -0,0 +1,7 @@
+function * handleGenerator (){
+   let name = yield "didier";
+
+   console.log(`this is the function that handle the variable created 
+    and also we are going  some data using dot next method
+    `);
+}
