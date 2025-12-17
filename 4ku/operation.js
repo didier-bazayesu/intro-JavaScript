d52273230571f2924e624b3684e322da4f310a3a
didier-bazayesu <didierbazayesu@gmail.com>
2025-12-17 23:39:28 +0200

    feat: result

--- 4ku/operation.js
+++ 4ku/operation.js
@@ -7,6 +7,10 @@ let add = (a,b) => a+b;
 let subtract = (a,b) => a-b;
 let multiply = (a,b) => a*b;
 let divide = (a,b) => a/b;
+console.log(operation(10,5,add));
+console.log(operation(10,5,subtract));
+console.log(operation(10,5,multiply));
+console.log(operation(10,5,divide));
 
 
 
