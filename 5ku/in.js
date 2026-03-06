82d31c60536f6a4115b6fae1017d35b68ccc4ff8
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-06 21:34:50 +0200

    feat: getting vowel in the string

---
+++ 5ku/in.js
@@ -0,0 +1,44 @@
+// function getCount(str) {
+//   let count =0;
+//   str.split('').map(elem=> {
+//     if('a,u,o,e,i'.includes(elem)) count++;
+//   })
+//   return count;
+// }
+
+// // console.log(getCount("emmy"))
+
+// const str="emmyi";
+
+// const arr=["i","u","o","a","e"];
+// let count=0;
+// for (let index = 0; index <= arr.length; index++) {
+//     if (str.includes(arr[index])) {
+//         count++;
+//     }  
+// }
+// console.log(count)
+
+// function getCount (string){
+//     let count =0;
+//      string.split('').map(elem => {
+//         if(elem == 'i'|| elem == 'a'|| elem == 'e'|| elem == 'u'|| elem == 'o') count ++;
+//     })
+//     return count;
+// }
+// console.log(getCount('didier'))
+
+
+// function findRepeated(arr){
+
+// }
+// console.log(findRepeated([1,1,1,1,1,2,3,3,2]));
+
+let arrayy=[1,1,1,1,1,2,3,3,2];
+
+let c=0;
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
--- Year2/Asyncexample/PromiseArray.js
+++ Year2/Asyncexample/PromiseArray.js
@@ -1,8 +1,2 @@
 const originalData = [1, 2, 3, 4, 5];
-const promiseArray = originalData.map((item) => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(item);
-        }, 1000);
-    });
-});
+
